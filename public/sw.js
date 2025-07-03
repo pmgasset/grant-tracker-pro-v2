a102dfac2d0db56c00c6cf610d487fd467cca3a0
pmgasset <david@pmgasset.com>
2025-07-03 11:45:32 -0400

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+// Grant Tracker Pro - Service Worker
+const CACHE_NAME = 'grant-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
