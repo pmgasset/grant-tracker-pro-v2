42bafe99215c122d3dc29671269b3a0561c6d71f
pmgasset <david@pmgasset.com>
2025-07-05 13:05:45 -0400

    Create test.js

---
+++ functions/test.js
@@ -0,0 +1,12 @@
+export async function onRequestGET() {
+  return new Response(JSON.stringify({
+    message: "Pages Functions are working!",
+    timestamp: new Date().toISOString(),
+    path: "Direct function test"
+  }), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
+}
